993734618c50374eeb03ba3056ade271d9a8441f
Maxim Melnik <melnik.max00@gmail.com>
2024-02-13 11:23:46 +0200

    Add Tailwind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className='text-3xl underline'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
